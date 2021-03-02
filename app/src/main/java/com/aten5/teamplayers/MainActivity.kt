23e1c1220a82424d5f44cddd9776bebcad11a053
dami <damixyz@outlook.com>
2021-03-02 20:54:42 +0000

    convert to binding layout

--- app/src/main/java/com/aten5/teamplayers/MainActivity.kt
+++ app/src/main/java/com/aten5/teamplayers/MainActivity.kt
@@ -2,10 +2,18 @@ package com.aten5.teamplayers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.databinding.DataBindingUtil
+import com.aten5.teamplayers.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return super.onCreateOptionsMenu(menu)
     }
 }
